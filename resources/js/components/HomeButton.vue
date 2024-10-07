4ebab19b967db7997fede01cad27db53087f7a07
HanJiBeom <a01034378289@gmail.com>
2024-10-07 15:38:10 +0900

    Revert "first commit"
    This reverts commit f57e8b8f30ee81cb396a358eb5b858cc90dfdd9f.

--- resources/js/components/HomeButton.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="my-4">
-      <Link href="/" class="bg-gray-500 text-white px-4 py-2 rounded">홈페이지로 이동</Link>
-    </div>
-  </template>
-  
-  <script setup>
-  import { Link } from '@inertiajs/vue3'
-  </script>
